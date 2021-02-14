a64347049f692dc1f657bbc3275262c4c49f2149
mitfleg <58234693+mitfleg@users.noreply.github.com>
2021-02-14 10:13:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+$(document).ready(function(){
+        $(".menu-right").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+            //анимируем переход на расстояние - top за 1500 мс
+            jQuery('body,html').animate({scrollTop: top}, 1500);
+        });
+        $(".btn-link").click(function(){
+            if ($('.btn-link').hasClass('collapsed')){
+                $('.btn-link').text('Скрыть');
+            }
+            else{
+                $('.btn-link').text('Далее');
+            }
+        });
+        $(".burger").click(function(){
+            $(".burger").toggleClass('active_burg')
+            $(".nav").toggleClass('active')
+            $(".programm").toggleClass('active_swipe')
+            if($('.burger').hasClass('active_burg')){
+                $('html, body').css('overflow', 'hidden');
+            }
+            else{
+                $('html, body').css('overflow', '')
+            }
+            $(".link-main").toggleClass('link')
+            $(".link").click(function(){
+                $('html, body').css('overflow', '')
+            });
+        });   
+         
+});
+
+    
